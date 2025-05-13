d74013fde80bfb425ee8d5b9b59919020a1a95c2
sustechailab <anonymauth@anonymauth.com>
2025-05-13 12:04:20 +0800

    Initial commit

--- update.sh
+++ update.sh
@@ -1,8 +1,9 @@
-git checkout --orphan TEMP_BRANCH
+#git checkout --orphan TEMP_BRANCH
 git add --all
 git commit -am "Initial commit"
-git branch -D master
-git branch -m master
+#git branch -D master
+#git branch -m master
 git gc
 git config --global http.postBuffer 1157286400
-git push -f origin master
+#git push -f origin master
+git push -f origin main
